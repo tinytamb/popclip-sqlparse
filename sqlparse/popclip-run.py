474b846be16a37f1788375b1719d85d8c985159a
mz <>
2020-01-04 16:47:58 +0900

    Rename sqlparse.py to popclip-run.py

---
+++ sqlparse/popclip-run.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+import sys
+import os
+current_dir = os.path.dirname(os.path.abspath(__file__))
+sys.path.append(str(current_dir) + '/lib')
+import sqlparse
+
+query = os.environ['POPCLIP_TEXT']
+print(sqlparse.format(query, reindent=True, keyword_case='upper'))
--- sqlparse/sqlparse.py
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env python3
-import os
-
-query = os.environ['POPCLIP_TEXT']
-# print("["+query+"]")
