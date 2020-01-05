cc01bcf9f572023c2594b708cfd4f9d69c4dfc8e
mz <>
2020-01-05 09:34:45 +0900

    fix

--- sqlparse/popclip-run.py
+++ sqlparse/popclip-run.py
@@ -5,5 +5,5 @@ current_dir = os.path.dirname(os.path.abspath(__file__))
 sys.path.append(str(current_dir) + '/lib')
 import sqlparse
 
-query = os.environ['POPCLIP_TEXT']
+query = os.environ.get('POPCLIP_TEXT', '')
 print(sqlparse.format(query, reindent=True, keyword_case='upper'))
