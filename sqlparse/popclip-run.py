6b8c6876fade199e87367131f3fb56de4d67152f
mz <>
2020-01-06 20:58:03 +0900

    Unicode NFD to NFC

--- sqlparse/popclip-run.py
+++ sqlparse/popclip-run.py
@@ -1,9 +1,11 @@
 #!/usr/bin/env python3
 import sys
 import os
+import unicodedata
 current_dir = os.path.dirname(os.path.abspath(__file__))
 sys.path.append(str(current_dir) + '/lib')
 import sqlparse
 
-query = os.environ.get('POPCLIP_TEXT', '')
+text = os.environ.get('POPCLIP_TEXT', '')
+query = unicodedata.normalize('NFC', text)
 print(sqlparse.format(query, reindent=True, keyword_case='upper'))
